838543d8b8f3a0377fcdc17b3370dd3ff75ba50b
MazenEllithy98 <mazen.a.ellethy@gmail.com>
2024-04-27 09:07:22 +0300

    adding pages and making ui responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import SignInForm from "./_auth/forms/SignInForm";
 import SignUpForm from "./_auth/forms/SignUpForm";
-import { Home } from "./_root/pages";
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPosts, PostDetails, Profile, Saved, UpdateProfile } from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
@@ -19,6 +19,15 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />}/>
+          <Route path="/saved" element={<Saved />}/>
+          <Route path="/all-users" element={<AllUsers />}/>
+          <Route path="/create-post" element={<CreatePost />}/>
+          <Route path="/update-post/:id" element={<EditPost />}/>
+          <Route path="/posts/:id" element={<PostDetails />}/>
+          <Route path="/profile/:id/*" element={<Profile />}/>
+          <Route path="/update-profile/:id" element={<UpdateProfile />}/>
+          <Route path="/liked-posts" element={<LikedPosts />}/>
         </Route>
       </Routes>
 
--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>Bottombar</div>
-  )
-}
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              width={16}
+              height={16}
+              src={link.imgURL}
+              alt={link.label}
+              className={`group-hover:invert-white ${
+                isActive && "invert-white"
+              }`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Bottombar
+export default Bottombar;
